4b3adc2c61b7db20833c34e907743965272e3e92
Eva Larumbe <evalarumbe@gmail.com>
2018-09-16 20:47:42 +0200

    ESLint - Allow long lines of code

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   extends: 'react-tools',
   extends: 'airbnb',
   rules: {
+    /* Allow long lines since this project includes long body text. Wrap if necessary. */
+    'max-len': 'off',
     'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
     /* Overwrite the default forbidden entities array ['>', '"', '\'', '}'] */
     'react/no-unescaped-entities': ['error', { 'forbid': ['>', '}'] }],
