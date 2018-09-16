5dcf4fbed4fec6e68628e78280a2ae1c85edbb66
Eva Larumbe <evalarumbe@gmail.com>
2018-09-16 22:13:26 +0200

    Fix linter error

--- src/containers/Bio.js
+++ src/containers/Bio.js
@@ -6,7 +6,7 @@ export default () => (
     <h2>Once upon a weird...</h2>
     <p className="callout">
       Labrysinthe is voluntarily committed to the institution of psytrance and stands for better machines, brighter knowledge, bolder music, and the blissful ecstacies of the beat.
-      </p>
+    </p>
     <p>
       Labrysinthe is Natalia Lincoln, who took the scenic route to psytrance -- first through classical training, then joining gothic/apocalyptic folk band Unto Ashes (NYC) as premium purveyor of keyboards, vocals, and gloom.
       Natalia moved to Berlin in 2008 to taste the delightfully twisted freakquencies of electronic psychedelica, at which point Labrysinthe was born into this world that could neither spell her name nor name her spell.
