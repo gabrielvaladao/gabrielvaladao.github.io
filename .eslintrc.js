1812b0997403c519449531cecf11d0e8a89850e3
Eva Larumbe <evalarumbe@gmail.com>
2018-09-16 22:04:40 +0200

    ESLint - Only enforce parens around args as needed

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   rules: {
     /* Allow long lines since this project includes long body text. Wrap if necessary. */
     'max-len': 'off',
+    'arrow-parens': ["error", "as-needed"],
     'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
     /* Overwrite the default forbidden entities array ['>', '"', '\'', '}'] */
     'react/no-unescaped-entities': ['error', { 'forbid': ['>', '}'] }],
