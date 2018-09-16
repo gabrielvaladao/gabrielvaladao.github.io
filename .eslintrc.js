d9c4c6164b9fdbdda79c484e0ae236c0669d150f
Eva Larumbe <evalarumbe@gmail.com>
2018-09-16 22:12:55 +0200

    ESLint - Allow multiple JSX expressions per line

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,10 @@ module.exports = {
   rules: {
     /* Allow long lines since this project includes long body text. Wrap if necessary. */
     'max-len': 'off',
-    'arrow-parens': ["error", "as-needed"],
+    'arrow-parens': ['error', 'as-needed'],
     'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
     /* Overwrite the default forbidden entities array ['>', '"', '\'', '}'] */
     'react/no-unescaped-entities': ['error', { 'forbid': ['>', '}'] }],
+    'react/jsx-one-expression-per-line': 'off',
   }
 }
