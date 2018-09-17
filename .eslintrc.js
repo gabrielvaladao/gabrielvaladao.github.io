b109e1f7615cd8254587a81bd9fe1b670da725b3
Eva Larumbe <evalarumbe@gmail.com>
2018-09-17 16:24:23 +0200

    ESLint - Allow funcs to be used before they're defined

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     /* Allow long lines since this project includes long body text. Wrap if necessary. */
     'max-len': 'off',
     'arrow-parens': ['error', 'as-needed'],
+    'no-use-before-define': ['error', { 'functions': false, 'classes': true }],
     'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
     /* Overwrite the default forbidden entities array ['>', '"', '\'', '}'] */
     'react/no-unescaped-entities': ['error', { 'forbid': ['>', '}'] }],
