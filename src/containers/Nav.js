bf3b0199c01f51c65da6b0b06f6251536ad18af2
Eva Larumbe <evalarumbe@gmail.com>
2018-10-04 02:46:26 +0200

    WIP - Add markup for mobile menu

--- src/containers/Nav.js
+++ src/containers/Nav.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import { Link } from 'react-static';
+import { Link, NavLink } from 'react-static';
 
+/* TODO: use icons for nav buttons */
 export default () => (
-  <nav aria-label="primary">
-    <Link exact to="/">Home</Link>
-    <Link to="/shows">Shows</Link>
-    <Link to="/music">Music</Link>
-    <Link to="/bio">Bio</Link>
-    <Link to="/press">Press</Link>
-    <Link to="/contact">Contact</Link>
-  </nav>
+  <React.Fragment>
+    <a
+      href="#main-menu"
+      id="main-menu-toggle"
+      role="button"
+      className="menu-toggle"
+      aria-expanded="false"
+      aria-controls="main-menu"
+      aria-label="Open the main menu"
+    >
+      <span className="sr-only">Open the main menu</span>
+      <span aria-hidden="true">=</span>
+    </a>
+
+    <nav aria-label="primary">
+
+      <a
+        href="#main-menu-toggle"
+        id="main-menu-close"
+        role="button"
+        className="menu-close"
+        aria-expanded="false"
+        aria-controls="main-menu"
+        aria-label="Close the main menu"
+      >
+        <span className="sr-only">Close the main menu</span>
+        <span aria-hidden="true">x</span>
+      </a>
+
+      <ul>
+        <li><NavLink exact to="/">Home</NavLink></li>
+        <li><NavLink to="/shows">Shows</NavLink></li>
+        <li><NavLink to="/music">Music</NavLink></li>
+        <li><NavLink to="/bio">Bio</NavLink></li>
+        <li><NavLink to="/press">Press</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
+      </ul>
+
+    </nav>
+
+    <a href="#main-menu-toggle" className="backdrop" tabIndex="-1" aria-hidden="true" hidden />
+  </React.Fragment>
 );
