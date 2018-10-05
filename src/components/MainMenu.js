8286e95726d056efce88fa719337c785a3eaebaa
Eva Larumbe <evalarumbe@gmail.com>
2018-10-05 16:30:37 +0200

    Get first styled-components experiment working

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-static';
+import styled from 'styled-components';
 
+const MainMenu = styled.ul`
+  color: #cc0;
+`;
 export default () => (
-  <ul>
+  <MainMenu>
     <li><NavLink exact to="/">Home</NavLink></li>
     <li><NavLink to="/shows">Shows</NavLink></li>
     <li><NavLink to="/music">Music</NavLink></li>
     <li><NavLink to="/bio">Bio</NavLink></li>
     <li><NavLink to="/press">Press</NavLink></li>
     <li><NavLink to="/contact">Contact</NavLink></li>
-  </ul>
+  </MainMenu>
 );
