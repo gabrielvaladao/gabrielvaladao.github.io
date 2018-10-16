97b4c972791183a144749951702bf3943437f454
Eva Larumbe <evalarumbe@gmail.com>
2018-10-17 01:47:01 +0200

    Update color of play buttons

--- src/containers/Music.js
+++ src/containers/Music.js
@@ -11,7 +11,7 @@ export default withRouteData(() => (
         <Track
           title="Fire Loves Sugar (Teaser)"
           soundCloudEmbed={[
-            <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/489549831&color=%2388e8b8&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>,
+            <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/489549831&color=%235f1f5f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>,
           ]}
         />
       </li>
@@ -19,7 +19,7 @@ export default withRouteData(() => (
         <Track
           title="Shake the Dust (Teaser)"
           soundCloudEmbed={[
-            <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/368663576&color=%23663399&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>,
+            <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/368663576&color=%2308000f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>,
           ]}
         />
       </li>
@@ -27,17 +27,19 @@ export default withRouteData(() => (
         <Track
           title="Delicious Monster EP - Previews"
           soundCloudEmbed={[
-            <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/343832514&color=%2388e8b8&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>,
+            <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/343832514&color=%23ef1f4f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>,
           ]}
-          ctaText="Download on Beatport"
-          ctaUrl="https://www.beatport.com/release/delicious-monster/2106224"
+          cta={{
+            text: 'Download on Beatport',
+            url: 'https://www.beatport.com/release/delicious-monster/2106224',
+          }}
         />
       </li>
       <li>
         <Track
           title="Far and Away"
           soundCloudEmbed={[
-            <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/342470222&color=%23663399&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>,
+            <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/342470222&color=%231f9f9f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>,
           ]}
         />
       </li>
