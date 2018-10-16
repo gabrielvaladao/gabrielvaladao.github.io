d646d86ffad6eb5ac406ced93a150815a60cfff2
Eva Larumbe <evalarumbe@gmail.com>
2018-10-17 01:46:28 +0200

    Customize 404

--- src/containers/404.js
+++ src/containers/404.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-static';
 
 export default () => (
   <div>
-    <h1>404 - Oh no's! We couldn't find that page :(</h1>
+    <h1>404 - The page you are trying to access is not of this dimension. Please try another link or head <Link exact to="/">Home</Link></h1>
   </div>
 );
