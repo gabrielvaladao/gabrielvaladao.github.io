9ab5dfc092526bd2c0abffd58429b55a78b45beb
Eva Larumbe <evalarumbe@gmail.com>
2018-11-02 02:39:00 +0100

    Music: Add unique keys to list items

--- src/containers/Music.js
+++ src/containers/Music.js
@@ -19,27 +19,27 @@ export default withRouteData(() => (
     <H1>Music</H1>
     <p>Select downloads are available on <StyledLink to="https://www.beatport.com/artist/labrysinthe/640674/tracks">Labrysinthe's Beatport</StyledLink></p>
     <Ul>
-      <Li>
+      <Li key="fire-loves-sugar-teaser">
         <Track
           title="Fire Loves Sugar (Teaser)"
           soundCloudEmbed={[
-            <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/489549831&color=%235f1f5f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>,
+            <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/489549831&color=%235f1f5f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>,
           ]}
         />
       </Li>
-      <Li>
+      <Li key="shake-the-dust-teaser">
         <Track
           title="Shake the Dust (Teaser)"
           soundCloudEmbed={[
-            <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/368663576&color=%2308000f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>,
+            <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/368663576&color=%2308000f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>,
           ]}
         />
       </Li>
-      <Li>
+      <Li key="delicious-monster-ep-previews">
         <Track
           title="Delicious Monster EP - Previews"
           soundCloudEmbed={[
-            <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/343832514&color=%23ef1f4f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>,
+            <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/343832514&color=%23ef1f4f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>,
           ]}
           cta={{
             text: 'Download on Beatport',
@@ -47,11 +47,11 @@ export default withRouteData(() => (
           }}
         />
       </Li>
-      <Li>
+      <Li key="far-and-away">
         <Track
           title="Far and Away"
           soundCloudEmbed={[
-            <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/342470222&color=%231f9f9f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>,
+            <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/342470222&color=%231f9f9f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>,
           ]}
         />
       </Li>
