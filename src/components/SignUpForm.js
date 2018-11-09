f7435cec630492627ba6d0db8e2044a5cc1a392f
Eva Larumbe <evalarumbe@gmail.com>
2018-11-09 11:52:08 +0100

    Remove broken reCAPTCHA implementation

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -102,8 +102,6 @@ export default class SignUpForm extends React.Component {
               />
             </label>
 
-            <div data-netlify-recaptcha />
-
             <Button type="submit">Sign up</Button>
 
           </form>
