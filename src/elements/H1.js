87f1692e8fd3898cc05b922e2f5bc3c5afd790ed
Eva Larumbe <evalarumbe@gmail.com>
2018-11-16 00:09:48 +0100

    H1: tweak style

--- src/elements/H1.js
+++ src/elements/H1.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 //
 const H1 = styled.h1`
+  text-align: center;
   font-family: ${props => props.theme.type.h1.fontFamily};
   font-weight: ${props => props.theme.type.h1.fontWeight};
   font-size: ${props => props.theme.type.h1.fontSize};
