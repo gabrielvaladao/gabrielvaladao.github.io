c12384bcc89687dabb2eebea50b0d94959c13a85
Eva Larumbe <evalarumbe@gmail.com>
2018-11-20 00:51:49 +0100

    Footer: add padding

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+//
 import Menu from '../components/Menu';
 import Newsletter from '../components/Newsletter';
 import Socials from '../components/Socials';
 
+const Footer = styled.footer`
+  padding: 1.2rem 2rem 2rem 2rem;
+
+  @media (min-width: 576px) {
+    padding: 1.2rem 4rem 2rem 4rem;
+  }
+`;
+
 export default () => (
-  <footer id="newsletter">
+  <Footer id="newsletter">
     <Newsletter />
     <Socials />
     <nav>
       <Menu />
     </nav>
     <div role="contentinfo">&copy; 2018 Labrysinthe</div>
-  </footer>
+  </Footer>
 );
