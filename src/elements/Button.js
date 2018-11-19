6a0309317957342d5496b4551ca3703576cce582
Eva Larumbe <evalarumbe@gmail.com>
2018-11-19 12:26:41 +0100

    Update button styles

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -4,9 +4,12 @@ import styled from 'styled-components';
 //
 
 /**
- * TODO: calculate padding based on Theme
- * Use same padding for Newsletter > Input
+ * TODO:
+ *  - Calculate padding based on Theme
+ *  - Use same padding for Newsletter > Input
+ *  - Define border-radius in Theme
  * */
+
 const Button = styled.button`
   -webkit-appearance: none;
   font-family: ${props => props.theme.type.compact.fontFamily};
@@ -14,6 +17,10 @@ const Button = styled.button`
   text-align: center;
   letter-spacing: -0.1ch;
   padding: 0.2rem 0.5rem 0.3rem 0.5rem;
+  background-color: #08000f;
+  color: #eeedef;
+  border: 2px solid #6fdfdf;
+  border-radius: 3px;
 
   &:active {
     background-color: #1f9f9f;
