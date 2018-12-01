bdf1f07c2a1efaf3db3530c21bd388d576b01c4d
Eva Larumbe <evalarumbe@gmail.com>
2018-12-01 15:56:41 +0100

    Update copy

--- src/containers/Shows.js
+++ src/containers/Shows.js
@@ -49,7 +49,7 @@ export default withRouteData(({ id, ...props }) => (
             venueAddress2="Entrance through Café Liberación"
             venueGMapsUrl="https://www.google.com/maps/place/Kinzig9/@52.5137004,13.4663543,15z/data=!4m5!3m4!1s0x0:0x5eb804e91e785a6b!8m2!3d52.5137004!4d13.4663543"
             fbEventUrl="https://www.facebook.com/events/192580104960799/"
-            ctaText="TICKETS (Free)"
+            ctaText="TICKETS (Free&nbsp;online)"
             ctaUrl="https://www.eventbrite.com.au/e/pms-the-l-night-party-tickets-52068345824"
           />
         </Li>
