2095b1cfc33d0f8122053547672f118b22be99b0
Eva Larumbe <evalarumbe@gmail.com>
2018-12-01 16:10:00 +0100

    Initialize top-of-page styles

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -5,13 +5,17 @@ import styled from 'styled-components';
 //
 import MainNav from '../components/MainNav';
 //
+  /**
+   * overlayDark border is invisible at first.
+   * Color changes on scroll
+   */
 const Header = styled.header`
   position: sticky;
   z-index: 900;
   top: 0;
   height: ${props => props.theme.space.three};
   background: ${props => props.theme.color.overlayDark};
-  border-bottom: 1px solid ${props => props.theme.color.overlayLight};
+  border-bottom: 1px solid ${props => props.theme.color.overlayDark};
 `;
 
 export default props => (
