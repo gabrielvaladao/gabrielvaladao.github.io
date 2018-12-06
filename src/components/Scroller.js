c9c7c3e4021194009c9484284ef548b8574c0b56
Eva Larumbe <evalarumbe@gmail.com>
2018-12-06 19:55:06 +0100

    Move non-pages out of containers dir (only affects Scroller)

--- src/components/Scroller.js
+++ src/components/Scroller.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 import Waypoint from 'react-waypoint';
-import Header from './Header';
+import Header from '../containers/Header';
 
 class Scroller extends Component {
   constructor(props) {
