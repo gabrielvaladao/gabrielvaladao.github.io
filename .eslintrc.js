ecf01835a569ea5947f4a8bbd8e67606c0f1f170
Eva Larumbe <evalarumbe@gmail.com>
2018-12-09 23:19:11 +0100

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,18 @@
 module.exports = {
-  parser: "babel-eslint",
-  extends: ["airbnb", "prettier", "react-tools"],
-  plugins: ["prettier"],
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier', 'react-tools'],
+  plugins: ['prettier'],
   rules: {
-    "prettier/prettier": ["error"],
+    'prettier/prettier': ['error'],
     /* Allow long lines since this project includes long body text. Wrap if necessary. */
-    "max-len": "off",
-    "arrow-parens": ["error", "as-needed"],
-    "no-use-before-define": ["error", { functions: false, classes: true }],
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    'max-len': 'off',
+    /* Override Airbnb no-restricted-syntax to allow ForOfStatement to iterate over NodeLists returned by querySelectorAll */
+    'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
+    'arrow-parens': ['error', 'as-needed'],
+    'no-use-before-define': ['error', { functions: false, classes: true }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     /* Overwrite the default forbidden entities array ['>', '"', '\'', '}'] */
-    "react/no-unescaped-entities": ["error", { forbid: [">", "}"] }],
-    "react/jsx-one-expression-per-line": "off",
-  },
+    'react/no-unescaped-entities': ['error', { forbid: ['>', '}'] }],
+    'react/jsx-one-expression-per-line': 'off'
+  }
 };
