cfca49b04648eca6be3c19fd773c4365a0589b5e
Eva Larumbe <evalarumbe@gmail.com>
2018-12-09 17:03:28 +0100

    Refactor styles and error handling

--- src/containers/Newsletter.js
+++ src/containers/Newsletter.js
@@ -8,6 +8,27 @@ import H1 from '../elements/H1';
 import H2 from '../elements/H2';
 import Button from '../elements/Button';
 
+/* UTILITIES */
+
+class EmailError extends Error {
+  name = 'EmailError';
+  message = 'Please enter a valid email address';
+}
+
+class ReCAPTCHAError extends Error {
+  name = 'ReCAPTCHAError';
+  message = "Please check the box to confirm you're human";
+}
+
+/* Encode data for the form's POST resquest */
+function encode(data) {
+  return Object.keys(data)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&');
+}
+
+/* STYLES */
+
 const Wrapper = styled.section`
   text-align: center;
 `;
@@ -18,26 +39,25 @@ const P = styled.p`
 
 /* TODO: calculate ch values and max-width based on Theme */
 const Form = styled.form`
-  /* TODO: remove previous grid styles */
-  display: block;
-  grid-template-columns: min-content minmax(0, 1fr) max-content;
-  grid-template-rows: min-content minmax(0, min-content);
-  column-gap: 0.5ch;
+  display: grid;
   margin: 0 auto;
-  text-align: left;
+  grid-template-columns: minmax(0, min-content) minmax(0, min-content);
+  grid-template-rows: repeat(6, min-content);
+  grid-column-gap: 1ch;
 
+  /* This is what Google generates automatically */
+  max-width: 304px;
+
+  /* TODO: do we need this? */
   @media (min-width: 576px) {
     max-width: 500px;
-    grid-template-columns: min-content minmax(6ch, 1fr) min-content;
-    grid-template-rows: min-content minmax(0, min-content);
   }
 `;
 
+/* TODO: width is hacky. do this with box-sizing or borders */
 const Label = styled.label`
-  grid-row: 1 / 2;
   grid-column: 1 / 2;
-  display: flex;
-  align-items: center;
+  grid-row: 1 / 2;
 `;
 
 /**
@@ -45,46 +65,53 @@ const Label = styled.label`
  * Use same padding for Button
  * */
 const Input = styled.input`
-  grid-row: 1 / 2;
   grid-column: 2 / 3;
-  border: 0;
+  grid-row: 1 / 2;
+  /* border: 1px solid #d3d3d3; */
+  border-radius: 3px;
   letter-spacing: -0.1ch;
   padding: 0.2rem 0.5rem;
-  box-sizing: border-box;
+  /* margin-left: 1ch; */
+  /* flex-grow: 1; */
+`;
 
-  @media (min-width: 576px) {
-    grid-row: 1 / 2;
-    grid-column: 2 / 3;
-  }
+const StyledReCAPTCHA = styled(ReCAPTCHA)`
+  grid-column: 1 / 3;
+  grid-row: 3 / 4;
+  margin: 0 auto;
 `;
 
 const Submit = styled(Button)`
-  grid-row: 1 / 2;
-  grid-column: 3 / 4;
-  display: inline-block;
-  box-sizing: border-box;
+  /* display: inline-block; */
+  /* flex-grow: 1; */
+  grid-column: 1 / 3;
+  grid-row: 5 / 6;
   padding: ${({ theme }) => theme.space.quarter};
-
-  @media (min-width: 576px) {
-    grid-row: 1 / 2;
-    grid-column: 3 / 4;
-  }
 `;
 
-/* TODO use a margin from theme sizes */
 const ErrorMessage = styled.p`
   visibility: hidden;
-  grid-row: 2 / 3;
-  grid-column: 2 / 4;
   font-size: ${({ theme }) => theme.type.small.fontSize};
   color: ${({ theme }) => theme.color.error};
   margin: ${({ theme }) => theme.space.quarter} 0;
+  text-align: left;
   white-space: normal;
+`;
 
-  @media (min-width: 576px) {
-    grid-row: 2 / 3;
-    grid-column: 2 / 4;
-  }
+const EmailErrorMessage = styled(ErrorMessage)`
+  grid-column: 1 / 3;
+  grid-row: 2 / 3;
+`;
+
+const ReCAPTCHAErrorMessage = styled(ErrorMessage)`
+  grid-column: 1 / 3;
+  grid-row: 4 / 5;
+`;
+
+const GenericErrorMessage = styled(ErrorMessage)`
+  grid-column: 1 / 3;
+  /* last row before submit button */
+  grid-row: 4 / 5;
 `;
 
 const Thanks = styled.div`
@@ -93,18 +120,12 @@ const Thanks = styled.div`
   }
 `;
 
-function encode(data) {
-  return Object.keys(data)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-    .join('&');
-}
-
 class Newsletter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
     this.handleChange = this.handleChange.bind(this);
-    this.handleRecaptcha = this.handleRecaptcha.bind(this);
+    this.handleReCAPTCHA = this.handleReCAPTCHA.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -112,7 +133,7 @@ class Newsletter extends React.Component {
     this.setState({ [e.target.name]: e.target.value });
   }
 
-  handleRecaptcha(response) {
+  handleReCAPTCHA(response) {
     this.setState({ 'g-recaptcha-response': response });
   }
 
@@ -121,10 +142,10 @@ class Newsletter extends React.Component {
     e.preventDefault();
     try {
       if (!this.state.user_email || !isEmail(this.state.user_email)) {
-        throw new Error('Please enter a valid email address');
+        throw new EmailError();
       }
       if (!this.state['g-recaptcha-response']) {
-        throw new Error("Please check the box to confirm you're human");
+        throw new ReCAPTCHAError();
       }
       const form = e.target;
 
@@ -136,14 +157,14 @@ class Newsletter extends React.Component {
           ...this.state
         })
       })
-        .then(() => this.showThanks())
-        .catch(error => this.showError(error));
+        .then(() => this.displayThanks())
+        .catch(error => this.displayError(error));
     } catch (error) {
-      this.showError(error);
+      this.displayError(error);
     }
   }
 
-  showThanks() {
+  displayThanks() {
     if (typeof document !== 'undefined') {
       document.querySelector('#mc-sign-up').setAttribute('hidden', 'true');
       document.querySelector('#error').style.visibility = 'hidden';
@@ -152,14 +173,19 @@ class Newsletter extends React.Component {
     }
   }
 
-  showError(error) {
-    console.log('show error');
+  displayError(error) {
     if (typeof document !== 'undefined') {
-      console.log('document true');
-      const errorMessage = document.querySelector('#error');
+      let errorMessage;
+      if (error.name === 'EmailError') {
+        errorMessage = document.querySelector('#email-error');
+      } else if (error.name === 'ReCAPTCHAError') {
+        errorMessage = document.querySelector('#recaptcha-error');
+      } else {
+        errorMessage = document.querySelector('#error');
+      }
       errorMessage.textContent = error;
-      document.querySelector('#error').style.position = 'relative';
-      document.querySelector('#error').style.visibility = 'visible';
+      errorMessage.style.position = 'relative';
+      errorMessage.style.visibility = 'visible';
     }
   }
 
@@ -198,13 +224,15 @@ class Newsletter extends React.Component {
               onChange={this.handleChange}
               required
             />
-            <ErrorMessage id="error" />
+            <EmailErrorMessage id="email-error" />
 
-            <ReCAPTCHA
-              ref="recaptcha"
+            <StyledReCAPTCHA
               sitekey="6LdrhHkUAAAAAAWeHoi6gPJV6DiLoU3Cn9OptAWF"
-              onChange={this.handleRecaptcha}
+              onChange={this.handleReCAPTCHA}
             />
+            <ReCAPTCHAErrorMessage id="recaptcha-error" />
+
+            <GenericErrorMessage id="error" />
 
             <Submit type="submit">
               <span>Sign&nbsp;up</span>
