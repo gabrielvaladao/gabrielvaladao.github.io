7059407d2f1ee65cceb9b7707c5e9f7e6ac4416b
Eva Larumbe <evalarumbe@gmail.com>
2018-12-26 18:36:16 +0100

    Remove test data

--- static.config.js
+++ static.config.js
@@ -15,13 +15,6 @@ export default {
     title: 'Labrysinthe'
   }),
   getRoutes: async () => {
-    const { data: shows } = await axios.get('https://jsonplaceholder.typicode.com/posts');
-    const { data: tracks } = await axios.get('https://jsonplaceholder.typicode.com/posts');
-    const { data: pressReleases } = await axios.get('https://jsonplaceholder.typicode.com/posts');
-
-    /* TODO remove debugging log */
-    console.log(Contentful.bio);
-
     return [
       {
         path: '/',
@@ -29,31 +22,11 @@ export default {
       },
       {
         path: '/shows',
-        component: 'src/containers/Shows',
-        getData: () => ({
-          shows
-        }),
-        children: shows.map(show => ({
-          path: `/show/${show.id}`,
-          component: 'src/components/Show',
-          getData: () => ({
-            show
-          })
-        }))
+        component: 'src/containers/Shows'
       },
       {
         path: '/music',
-        component: 'src/containers/Music',
-        getData: () => ({
-          tracks
-        }),
-        children: tracks.map(track => ({
-          path: `/track/${track.id}`,
-          component: 'src/components/Track',
-          getData: () => ({
-            track
-          })
-        }))
+        component: 'src/containers/Music'
       },
       {
         path: '/bio',
