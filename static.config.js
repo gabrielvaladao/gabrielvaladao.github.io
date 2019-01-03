4a77ea284faa27742fa5d983e9205c6b683efc50
Eva Larumbe <evalarumbe@gmail.com>
2019-01-03 05:37:35 +0100

    Fix syntax error

--- static.config.js
+++ static.config.js
@@ -53,7 +53,7 @@ export default {
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131654117-1"></script>
         <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+        function gtag(){dataLayer.push(arguments)}
         gtag('js', new Date());
 
         gtag('config', 'UA-131654117-1', { 'anonymize_ip': true });
